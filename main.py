772c9e0c10f548e6a743e7dd248b2a025248c343
TimurBerdyyev <gorilazanimals@gmail.com>
2024-05-21 10:03:29 +0600

    добавление функции

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from reader import read_and_clean_excel
 from generator_html import generate_html_file
+import atexit
+import os
 
 class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -20,8 +22,18 @@ class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
         generate_html_file(wines_dict=wines_dict, special_offers=special_offers, age=10, year_suffix='лет')
         super().do_GET()
 
+def delete_index_html():
+    if os.path.exists('index.html'):
+        os.remove('index.html')
+
 if __name__ == '__main__':
     server_address = ('0.0.0.0', 8000)
     httpd = HTTPServer(server_address, CustomHTTPRequestHandler)
-    print(f'http://{server_address[0]}:{server_address[1]}')
-    httpd.serve_forever()
+    atexit.register(delete_index_html)
+    print(f'Server is running at http://{server_address[0]}:{server_address[1]}')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        httpd.server_close()
