24a095e9e42ca856c51d4898bc9ca5b3561a7594
TimurBerdyyev <gorilazanimals@gmail.com>
2024-05-24 12:24:50 +0600

    исправление ошибок

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from reader import read_and_clean_excel
 from generator_html import generate_html_file
+
 import atexit
 import os
 
+from collections import defaultdict
+
+
 def setup_wine_data():
     products = read_and_clean_excel('xlsx_file/wine3.xlsx')
-    wines_dict = {}
+    wines_dict = defaultdict(list)
     special_offers = set()
     
     for product in products:
         category = product['Категория']
-        if category not in wines_dict:
-            wines_dict[category] = []
         wines_dict[category].append(product)
         if product.get('Акция') == 'Выгодное предложение':
             special_offers.add(product['Название'])
     
     generate_html_file(wines_dict=wines_dict, special_offers=special_offers, age=10, year_suffix='лет')
+
 
 def cleanup():
     if os.path.exists('index.html'):
@@ -28,14 +31,8 @@ def main():
     server_address = ('0.0.0.0', 8000)
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
     atexit.register(cleanup)
-    print(f'Server is running at http://{server_address[0]}:{server_address[1]}')
-    
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        httpd.server_close()
+    print(f'Сайт доступен по адрессу  http://{server_address[0]}:{server_address[1]}')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
     main()
